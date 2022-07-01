f0bb569eaccf4d52c40d757ce9c1511c3d4c1ad8
Igor Carvalho <igor.carvalhox8@gmail.com>
2022-07-01 16:26:01 -0300

    simple customer api

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { Customer } from './entities/customer.entity';
+import { validate } from 'class-validator';
 
 @Injectable()
 export class CustomerService {
@@ -12,23 +13,51 @@ export class CustomerService {
     private readonly customerRepository: Repository<Customer>
   ) { }
 
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+  async create(createCustomerDto: CreateCustomerDto) {
+    await validate(createCustomerDto).then(errors => {
+      if (errors.length > 0) {
+        return 'validação não passou, erros: ' + errors;
+      }
+      else {
+        console.log('validação concluída');
+        this.customerRepository.save(createCustomerDto)
+        return 'Cliente salvo com sucesso'
+      }
+    })
   }
 
   findAll() {
-    return `This action returns all customer`;
+    return this.customerRepository.find()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} customer`;
+    return this.customerRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    await validate(updateCustomerDto).then(async errors => {
+      if (errors.length > 0) {
+        return 'Validação não passou'
+      }
+      else {
+        console.log('validação concluída');
+        const customer = await this.customerRepository.preload({
+          id: +id,
+          ...updateCustomerDto
+        })
+
+        if (!customer) throw new NotFoundException(`Cliente id: ${id} não encontrado`)
+        else return this.customerRepository.save(customer)
+      }
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
+  async remove(id: number) {
+    let customer = await this.customerRepository.findOne({ where: { id } })
+
+    if (!customer) {
+      throw new NotFoundException(`Cliente id: ${id} não encontrado`)
+    }
+    else return this.customerRepository.delete(customer)
   }
 }
--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,9 +1,21 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsString, Length } from "class-validator";
+import { CreateDateColumn, UpdateDateColumn } from "typeorm";
 
 export class CreateCustomerDto {
+  @Length(5, 50)
   @IsString()
   readonly name: string;
 
+  @IsEmail()
+  readonly email: string;
+
+  @Length(10, 11)
   @IsString()
   readonly phone: string;
+
+  @CreateDateColumn()
+  readonly createdAt: Date;
+
+  @UpdateDateColumn()
+  readonly updatedAt: Date;
 }
